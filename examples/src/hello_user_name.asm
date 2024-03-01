41841e014196a1e7ac4c7cd999e5574ca5b30f65
Daniil Kalinin <dan_kalinin_03@mail.ru>
2024-03-01 13:39:40 +0300

    Update asm code

--- examples/src/hello_user_name.asm
+++ examples/src/hello_user_name.asm
@@ -7,8 +7,17 @@ second_message:
 exclamation_point:
   .word '!'
 
-pointer:  ; Адрес печатаемого символа
-  .word 0
+first_message_pointer:
+  .word first_message
+
+second_message_pointer:
+  .word second_message
+
+read_name_pointer:
+  .word name
+
+write_name_pointer:
+  .word name
 
 end_str:
   .word 0
@@ -22,11 +31,8 @@ out_addr:
 _start:
 
   write_first_message:  ; Пишем первое сообщение
-    mov r0, first_message
-    mov pointer, r0  ; Кладем в pointer указатель на первый символ первого сообщения
-
     load_first_message_char:
-      mov r0, (pointer)
+      mov r0, (first_message_pointer)
       mov r1, r0
       mov r2, end_str
       sub r1, r2
@@ -35,34 +41,28 @@ _start:
 
     write_first_message_char:
       mov (out_addr), r0
-      mov r0, pointer
+      mov r0, first_message_pointer
       inc r0
-      mov pointer, r0
+      mov first_message_pointer, r0
       jmp load_first_message_char
 
   read_name:
-    mov r0, name
-    mov pointer, r0  ; Кладем в pointer указатель на первый символ имени
-
     read_name_char:
       mov r0, (in_addr)
 
     store_name_char:
-      mov (pointer), r0
+      mov (read_name_pointer), r0
       mov r1, end_str
       sub r0, r1
       jz write_second_message
-      mov r0, pointer
+      mov r0, read_name_pointer
       inc r0
-      mov pointer, r0
+      mov read_name_pointer, r0
       jmp read_name_char
 
   write_second_message:  ; Пишем второе сообщение
-    mov r0, second_message
-    mov pointer, r0  ; Кладем в pointer указатель на первый символ второго сообщения
-
     load_second_message_char:
-      mov r0, (pointer)
+      mov r0, (second_message_pointer)
       mov r1, r0
       mov r2, end_str
       sub r1, r2
@@ -71,17 +71,14 @@ _start:
 
     write_second_message_char:
       mov (out_addr), r0
-      mov r0, pointer
+      mov r0, second_message_pointer
       inc r0
-      mov pointer, r0
+      mov second_message_pointer, r0
       jmp load_second_message_char
 
   write_name:  ; Пишем имя
-    mov r0, name
-    mov pointer, r0  ; Кладем в pointer указатель на первый символ имени
-
     load_name_char:
-      mov r0, (pointer)
+      mov r0, (write_name_pointer)
       mov r1, r0
       mov r2, end_str
       sub r1, r2
@@ -90,13 +87,13 @@ _start:
 
     write_name_char:
       mov (out_addr), r0
-      mov r0, pointer
+      mov r0, write_name_pointer
       inc r0
-      mov pointer, r0
+      mov write_name_pointer, r0
       jmp load_name_char
 
   write_exclamation_point:  ; Пишем восклицательный знак
-    mov r0, (exclamation_point)
+    mov r0, exclamation_point
     mov (out_addr), r0
     hlt
 
